efc05a18271ced0a73c19a9b463b9df71300eee9
ElperdonXV <davidemartinografico@gmail.com>
2022-01-13 16:48:22 +0100

    working on header

--- src/App.vue
+++ src/App.vue
@@ -21,3 +21,6 @@ export default {
 }
 </script>
 
+<style lang="scss">
+@import "./assets/scss/style.scss";
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,42 @@
 <template>
-  <img src="../assets/img/dc-logo.png" alt="">
+    <div class="container">
+        <header>
+            <img src="../assets/img/dc-logo.png" alt="Dc Comics" class="logo">
+            <ul>
+                <li>
+                    <a href="">Characters</a>
+                </li>
+                <li>
+                    <a href="">Comics</a>
+                </li>
+                <li>
+                    <a href="">Movies</a>
+                </li>
+                <li>
+                    <a href="">Tv</a>
+                </li>
+                <li>
+                    <a href="">Games</a>
+                </li>
+                <li>
+                    <a href="">Collections</a>
+                </li>
+                <li>
+                    <a href="">Videos</a>
+                </li>
+                <li>
+                    <a href="">Fans</a>
+                </li>
+                <li>
+                    <a href="">News</a>
+                </li>
+                <li>
+                    <a href="">Shop</a>
+                </li>
+            </ul>
+        </header>
+    </div>
+    
 </template>
 
 <script>
@@ -11,6 +48,26 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    ul{
+        list-style: none;
+        display: flex;
+        gap: 10px;
+        li a {
+            text-decoration: none;
+            color: black;
+            font-weight: bold;
+            text-transform: uppercase;
+            font-size: 1.2em;
+        }
+    }
+    .logo{
+        width: 60px;
+    }
 </style>
